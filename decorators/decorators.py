0ac030e4f31032b274c9338733d28a00bbe395b1
Vladimir <baimieff.volodya1@yandex.ru>
2024-09-17 22:21:35 +0500

    Create link on wrapper
    Here create link on wrapper which is in function

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -3,10 +3,14 @@ def summ(a, b):
     return a + b
 
 def fout(func):
-    print("Start")
-    print("End")
-    return func
+    def wrapper(a, b):
+        print("Start")
+        result = func(a, b)
+        print("End")
+        return result
+    return wrapper
 
 
 if __name__ == "__main__":
-    print(fout(summ))
+    summ = fout(summ)
+    print(summ)
